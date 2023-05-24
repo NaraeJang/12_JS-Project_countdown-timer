cb4c1ea7d5722f69a6ffd6040ff77467ec189680
NaraeJang <j.naaaarae@gmail.com>
2023-05-23 22:07:37 -0400

    update js

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ const giveAway = document.querySelector(".giveaway");
 const items = document.querySelectorAll(".deadline-format h4");
 
 // get a giveaway date
-const futureDate = new Date(2024, 04, 24, 8, 30, 0);
+const futureDate = new Date(2023, 08, 29, 8, 30, 0);
 const year = futureDate.getFullYear();
 const hours = futureDate.getHours();
 const minutes = futureDate.getMinutes();
@@ -36,7 +36,59 @@ const date = futureDate.getDate();
 let month = futureDate.getMonth();
 month = months[month - 1];
 
-let futureday = futureDate.getDay();
-futureday = weekdays[futureday - 1];
+let weekday = futureDate.getDay();
+weekday = weekdays[weekday];
 
-giveAway.textContent = `Giveaway Ends On ${futureday}, ${date} ${month} ${year}, ${hours}:${minutes}am`;
+giveAway.textContent = `Giveaway Ends On ${weekday}, ${date} ${month} ${year}, ${hours}:${minutes}am`;
+
+// Calculate remaining days
+// future time in ms
+const futureTime = futureDate.getTime();
+
+function getRemainingTime() {
+  const today = new Date().getTime();
+  const t = futureTime - today;
+
+  // 1s = 1000ms
+  // 1m = 60s
+  // 1hr = 60mins
+  // 1day = 24hrs
+
+  // values in ms
+  const oneDay = 24 * 60 * 60 * 1000;
+  const oneHour = 60 * 60 * 1000;
+  const oneMinute = 60 * 1000;
+
+  let days = t / oneDay;
+  days = Math.floor(days);
+
+  let hours = (t % oneDay) / oneHour;
+  hours = Math.floor(hours);
+
+  let mins = (t % oneHour) / oneMinute;
+  mins = Math.floor(mins);
+
+  let secs = (t % oneMinute) / 1000;
+  secs = Math.floor(secs);
+
+  // set values array
+  const values = [days, hours, mins, secs];
+
+  function format(item) {
+    if (item < 10) {
+      return (item = `0${item}`);
+    }
+    return item;
+  }
+
+  items.forEach(function (item, index) {
+    item.innerHTML = format(values[index]);
+  });
+  if (t < 0) {
+    clearInterval(countdown);
+    deadline.innerHTML = `<h4 class="expired">Sorry, This giveaway has expired</h4>`;
+  }
+}
+
+// countdown
+let countdown = setInterval(getRemainingTime, 1000);
